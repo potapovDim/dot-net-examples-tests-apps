be30166af69d77fe7537ce4ff5fa7a1b2d24920d
potapovDim <potapovd3012@gmail.com>
2017-01-02 16:03:26 +0200

    simple tcp client .net

--- CalculatinMethods.cs
+++
@@ -1,7 +0,0 @@
-namespace someNewNameSpace{
-  class TestClass{
-    public int PLUSS(int a , int b){
-      return a+b;
-    }
-  }
-}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,55 @@
 ﻿using System;
-using someNewNameSpace;
-namespace InitialProgram
-{
-  public class CustomMethods
-  {
-    public double CalculateSum(double a, double b)
-    {
-      return a + b;
-    }
-    public double CalculateDiff(double a, double b)
-    {
-      return a - b;
-    }
-  }
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
 
-      var calcul = new TestClass();
-      double dododdoodod = calcul.PLUSS(10,12);
-      Console.WriteLine(dododdoodod);
-      Console.ReadKey();
+namespace HTTPServer
+{
+    class TCPHelper
+    {
+      private static TcpListener listener { get; set; }
+      private static bool accet {get; set;}
+      public static void StartServer(int port, bool _accet){
+        accet = _accet;
+        IPAddress adress = IPAddress.Parse("127.0.0.1");
+        listener = new TcpListener(adress, port);
+        listener.Start();
+        Console.WriteLine("Server start on=  {0}:{1} ", adress, port);
+      } 
+      public static void Listen(){
+        if(listener!=null &&  accet){
+          while(true){
+            Console.WriteLine("Listen for client");
+            var clientTasc = listener.AcceptTcpClientAsync();
+            if(clientTasc.Result!= null){
+              Console.WriteLine("Client conected , wait for data");
+              var client = clientTasc.Result;
+              string message = "";
+
+              while(message !=null && !message.StartsWith("quit")){
+                byte [] data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");
+                client.GetStream().Write(data, 0, data.Length);
+                byte [] buffer = new byte[1024];
+                client.GetStream().Read(buffer, 0, buffer.Length);
+                  
+                message = Encoding.ASCII.GetString(buffer);
+                Console.WriteLine(message);
+              }
+              Console.WriteLine("Connection close");
+              client.GetStream().Dispose();
+            }
+          }
+        }
+      }
     }
-  }
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            TCPHelper.StartServer(5678, true);  
+            TCPHelper.Listen();
+        }
+    }
 }
