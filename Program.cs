3d074c386efa89128ab127ccfd6864f7e5cf5daa
potapovDim <dmytrop@weblium.com>
2017-03-08 13:21:43 +0200

    refactor

--- Program.cs
+++ Program.cs
@@ -1,55 +1,52 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Sockets;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace HTTPServer
+namespace ConsoleApplication
 {
-    class TCPHelper
+ public class Startup
+{
+    public void Configure(IApplicationBuilder app)
     {
-      private static TcpListener listener { get; set; }
-      private static bool accet {get; set;}
-      public static void StartServer(int port, bool _accet){
-        accet = _accet;
-        IPAddress adress = IPAddress.Parse("127.0.0.1");
-        listener = new TcpListener(adress, port);
-        listener.Start();
-        Console.WriteLine("Server start on=  {0}:{1} ", adress, port);
-      } 
-      public static void Listen(){
-        if(listener!=null &&  accet){
-          while(true){
-            Console.WriteLine("Listen for client");
-            var clientTasc = listener.AcceptTcpClientAsync();
-            if(clientTasc.Result!= null){
-              Console.WriteLine("Client conected , wait for data");
-              var client = clientTasc.Result;
-              string message = "";
+        
+        var routeBuilder = new RouteBuilder(app);
+        
+        routeBuilder.MapGet("", context => context.Response.WriteAsync("Hello from root!"));
+        routeBuilder.MapGet("hello", context => context.Response.WriteAsync("Hello from /hello"));
+        routeBuilder.MapGet("hello/{name}", context => context.Response
+                                                              .WriteAsync($"Hello, {context.GetRouteValue("name")}"));
 
-              while(message !=null && !message.StartsWith("quit")){
-                byte [] data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");
-                client.GetStream().Write(data, 0, data.Length);
-                byte [] buffer = new byte[1024];
-                client.GetStream().Read(buffer, 0, buffer.Length);
-                  
-                message = Encoding.ASCII.GetString(buffer);
-                Console.WriteLine(message);
-              }
-              Console.WriteLine("Connection close");
-              client.GetStream().Dispose();
-            }
-          }
-        }
-      }
-    }
-    class Server
-    {
-        public static void Main(string[] args)
+        routeBuilder.MapGet("square/{number:int}", context =>
         {
-            TCPHelper.StartServer(5678, true);  
-            TCPHelper.Listen();
-        }
+            int number = Convert.ToInt32(context.GetRouteValue("number"));
+            return context.Response.WriteAsync($"The square of {number} is {number * number}");
+        });
+
+        routeBuilder.MapPost("post", context => context.Response.WriteAsync("Posting!"));
+
+        app.UseRouter(routeBuilder.Build());
+
     }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRouting();
+    }
+}
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+          .UseKestrel()
+          .UseStartup<Startup>()
+          .Build();
+
+      host.Run();
+    }
+  }
 }
